6f513164286a7f44a0ed79c6f52f5857e076aeb4
parth2804 <parthagarwal2804@gmail.com>
2026-06-01 02:15:34 +0530

    updated Q15

--- Day-4/Q15.c
+++ Day-4/Q15.c
@@ -1,69 +1,29 @@
 // Check if a number is an Armstrong number
 #include <stdio.h>
-int main()
+int main() 
 {
-    int num, original, temp, remainder, n = 0, result = 0;
+    int num, temp, digits = 0, result = 0;
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    original = num;
-
-    if (num == 0)
+    for (temp = num; temp != 0; temp /= 10) 
     {
-        n = 1;
+        digits++;
     }
-    else
+    for (temp = num; temp != 0; temp /= 10) 
     {
-        temp = num;
-
-        while (temp != 0)
+        int rem = temp % 10, p = 1;
+        for (int i = 0; i < digits; i++) 
         {
-            n++;
-            temp /= 10;
+            p *= rem;
         }
+        result += p;
     }
 
-    temp = num;
-
-    while (temp != 0)
-    {
-        remainder = temp % 10;
-
-        int power = 1;
-        int i = 1;
-
-        while (i <= n)
-        {
-            power *= remainder;
-            i++;
-        }
-
-        result += power;
-        temp /= 10;
-    }
-
-    if (num == 0)
-    {
-        printf("0^1 = 0\n");
-        result = 0;
-    }
+    if (result == num)
+        printf("%d is an Armstrong number\n", num);
     else
-    {
-        temp = num;
+        printf("%d is not an Armstrong number\n", num);
 
-        while (temp != 0)
-        {
-            remainder = temp % 10;
-            printf("%d^%d", remainder, n);
-            temp /= 10;
-            if (temp != 0)
-                {printf(" + ");}
-        }
-        printf(" = %d\n", result);
-    }
-    if (result == original)
-        printf("%d is an Armstrong number", num);
-    else
-        printf("%d is not an Armstrong number", num);
     return 0;
 }
