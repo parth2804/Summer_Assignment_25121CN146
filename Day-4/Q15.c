3b61b83435b7d8e40bd024deb549839039bdb0a6
parth2804 <parthagarwal2804@gmail.com>
2026-06-04 13:21:45 +0530

    Day-4 Uploaded

--- Day-4/Q15.c
+++ Day-4/Q15.c
@@ -2,28 +2,31 @@
 #include <stdio.h>
 int main() 
 {
-    int num, temp, digits = 0, result = 0;
-    printf("Enter a number: ");
-    scanf("%d", &num);
+int n, rem, sum = 0, temp, digits = 0;
+printf("Enter a number: ");
+scanf("%d", &n);
 
-    for (temp = num; temp != 0; temp /= 10) 
-    {
-        digits++;
-    }
-    for (temp = num; temp != 0; temp /= 10) 
-    {
-        int rem = temp % 10, p = 1;
-        for (int i = 0; i < digits; i++) 
-        {
-            p *= rem;
-        }
-        result += p;
-    }
+temp = n;
+while (temp != 0) 
+{
+    digits++;
+    temp = temp / 10;
+}
 
-    if (result == num)
-        printf("%d is an Armstrong number\n", num);
-    else
-        printf("%d is not an Armstrong number\n", num);
+temp = n;
+while (temp != 0) 
+{
+    rem = temp % 10;
+    int power = 1;
+    for (int i = 0; i < digits; i++)
+        power = power * rem;
+    sum = sum + power;
+    temp = temp / 10;
+}
+if (sum == n)
+    printf("%d is an Armstrong number\n", n);
+else
+    printf("%d is NOT an Armstrong number\n", n);
 
-    return 0;
+return 0;
 }
--- Day-4/Q16.c
+++ Day-4/Q16.c
@@ -1,30 +1,41 @@
 // Find Armstrong numbers in a given range
 #include <stdio.h>
+
 int main() 
 {
-    int beg, end;
-    printf("Enter range (beg end): ");
-    scanf("%d %d", &beg, &end);
+int low, high, n, rem, sum, temp, digits, power, i;
+printf("Enter range (low high): ");
+scanf("%d %d", &low, &high);
 
-    printf("Armstrong numbers between %d and %d:\n", beg, end);
+printf("Armstrong numbers between %d and %d:\n", low, high);
 
-    for (int i = beg; i <= end; i++) 
+for (n = low; n <= high; n++) 
+{
+    // count digits
+    digits = 0;
+    temp = n;
+    while (temp != 0) 
     {
-        int temp, digits = 0, result = 0;
-        for (temp = i; temp != 0; temp /= 10) 
-        {
-            digits++;
-        }
-        for (temp = i; temp != 0; temp /= 10) 
-        {
-            int rem = temp % 10, p = 1;
-            for (int j = 0; j < digits; j++) 
-            {
-                p *= rem;
-            }
-            result += p;
-        }
-        if (result == i) printf("%d\n", i);
+        digits++;
+        temp = temp / 10;
     }
-    return 0;
+
+    // calculate sum of powers
+    sum = 0;
+    temp = n;
+    while (temp != 0) 
+    {
+        rem = temp % 10;
+        power = 1;
+        for (i = 0; i < digits; i++)
+            power = power * rem;
+        sum = sum + power;
+        temp = temp / 10;
+    }
+
+    if (sum == n)
+        printf("%d\n", n);
+}
+
+return 0;
 }
